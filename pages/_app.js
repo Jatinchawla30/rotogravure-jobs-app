734ae09065212a361f062e89e079102481144681
Jatinchawla30 <JIGNESH.OLA77@GMAIL.COM>
2026-01-19 23:16:12 +0530

    Refactor MyApp to include user authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,32 @@
 import "../styles/globals.css";
-import { AuthProvider } from "../context/AuthContext";
+import { AuthProvider, useAuth } from "../context/AuthContext";
+import Layout from "../components/Layout";
 
-export default function MyApp({ Component, pageProps }) {
+function AppContent({ Component, pageProps }) {
+  const { user, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+
+  // Pages like login/signup should not show sidebar
+  if (
+    Component.noLayout ||
+    Component.name === "Login" ||
+    Component.name === "Signup"
+  ) {
+    return <Component {...pageProps} />;
+  }
+
+  return (
+    <Layout user={user}>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default function MyApp(props) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <AppContent {...props} />
     </AuthProvider>
   );
 }
